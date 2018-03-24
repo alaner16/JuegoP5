1b12c725425a1c63c829d1fce1cae2bf08e8c03a
ALAN <alanrivera750@gmail.com>
2018-03-23 20:44:50 -0700

    imagenes de jugadores cargadas

--- sketch.js
+++ sketch.js
@@ -1,31 +1,33 @@
 var count =0;
-var contCarro = 100;
+var contCarro = 1000;
 var contCarro2 = 100;
 var contCarro1 = 100;
 var img;
-var xx = 180;
+var xx = 140;
+
+function preload(){
+	img = loadImage("../carro.png")
+	avi = loadImage("../plane2.png")
+}
 function setup() {
   createCanvas(400, 400);
 	//rectMode(CENTER);
-	img = loadImage("./carro.jpg")
 	a=10;
 	b=10;
 	c=10;
 }
 
 function draw() {
-
-	image(img,200,200);
-	image(img, 0, height/2, img.width/2, img.height/2);
+	background(220);
 	
 	var x = width / 2;
 	var y = height - count; 
 	var yCarro = height - contCarro1;
 	var xCarro = height - contCarro2;
 	var zCarro = height - contCarro;
-	background(220);
+	
 	//jugador
-	rect (xx,280,40,70,20);
+	var jugador = image(img,xx,260);
 	//izquierda
 	rect (40,0,5,400);
 	rect (50,y-0,5,30);
@@ -69,40 +71,43 @@ function draw() {
 	count -= 3;
 	if(count < -200){
 		count = 0;
-		// contCarro = velocidad();
-		// contCarro2 = velocidad();
 	} 
 	//generacion de carros a la izquierda
-	var carroIzquierda = rect (80,yCarro-400,40,70);
+	var carroIzquierda = image (avi,70,yCarro-400);//,40,70);
 	contCarro1 -= a;
 	if(contCarro1 < -400){
 		contCarro1 = posicion();
 		a = velocidad();
 	}
 	//generacion de carros al centro
-	var carroCentro = rect (180,zCarro-400,40,70);
+	var carroCentro = image (avi,170,zCarro-400);//,40,70);
 	contCarro -= b;
 	if(contCarro < -400){
 		contCarro = posicion();
 		b = velocidad();
 	}
 	//generacion de carros a la derecha
-	var carroDerecha = rect (280,xCarro-400,40,70);
+	var carroDerecha = image (avi,270,xCarro-400);//,40,70);
 	contCarro2 -= c;
 	if(contCarro2 < -400){
 		contCarro2 = posicion();
 		c = velocidad();
 	}
+
+	var choke = collideRectRect(jugador.xx, jugador.y, 128, 128, carroCentro.xx, carroCentro.y,70,70);
+	if(choke){
+		console.log("hola");
+	}
 }
 
 function keyPressed(){
 	if (keyCode == 37){
-		if (xx!=80){
+		if (xx!=40){
 			xx -= 100;
 		}
 	}
 	if (keyCode == 39){
-		if (xx!=280){
+		if (xx!=240){
 			xx += 100;
 		}
 	}
@@ -112,10 +117,10 @@ function velocidad(){
 	return random(5,8);
 }
 function posicion(){
-	return random(100,200);
+	return random(100,300);
 }
 
-function generarCarro(){
+/* function generarCarro(){
 	
 	var carroDerecha = rect (280,contCarro-400,40,100);
 	contCarro -= 5;
@@ -141,4 +146,4 @@ function carroIzquierda(a,b,c,d){
 	rect (50,y-520,5,30);
 	rect (50,y-560,5,30);
 	rect (50,y-600,5,30);
-}
+} */
